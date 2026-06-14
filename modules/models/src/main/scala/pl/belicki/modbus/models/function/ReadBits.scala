7882fc75294fb748c470817edfcbbf11a24980c1
Marcin Belicki <marcin.belicki@protonmail.com>
2026-06-14 14:03:27 +0200

    correcting validation

--- modules/models/src/main/scala/pl/belicki/modbus/models/function/ReadBits.scala
+++ modules/models/src/main/scala/pl/belicki/modbus/models/function/ReadBits.scala
@@ -3,6 +3,6 @@ package pl.belicki.modbus.models.function
 trait ReadBits extends ReadAddressQuantity {
   this: ModbusFunction =>
 
-  final def validateQuantity(quantity: Int): Boolean = quantity < 2000 && quantity > 0
+  final def validateQuantity(quantity: Int): Boolean = quantity <= 2000 && quantity >= 1
 
 }
--- modules/models/src/main/scala/pl/belicki/modbus/models/function/ReadRegisters.scala
+++ modules/models/src/main/scala/pl/belicki/modbus/models/function/ReadRegisters.scala
@@ -3,6 +3,6 @@ package pl.belicki.modbus.models.function
 trait ReadRegisters extends ReadAddressQuantity {
   this: ModbusFunction =>
 
-  def validateQuantity(quantity: Int): Boolean = quantity < 125 && quantity > 0
+  def validateQuantity(quantity: Int): Boolean = quantity <= 125 && quantity >= 1
 
 }
