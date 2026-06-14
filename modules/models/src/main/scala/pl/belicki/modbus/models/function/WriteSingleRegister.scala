aa3884a0ac34f58d60a038662be6f51b384f61ce
Marcin Belicki <marcin.belicki@protonmail.com>
2026-06-14 14:13:39 +0200

    adding support for decoding WriteSingleRegister

--- modules/models/src/main/scala/pl/belicki/modbus/models/function/WriteSingleRegister.scala
+++ modules/models/src/main/scala/pl/belicki/modbus/models/function/WriteSingleRegister.scala
@@ -1,10 +1,39 @@
 package pl.belicki.modbus.models.function
 
+import pl.belicki.modbus.models.ExceptionCode
+
+import java.nio.ByteBuffer
+
 object WriteSingleRegister extends ModbusFunction(0x06) {
 
   case class Request(
       address: Int,
       value: Short
   ) extends super.Request
+
+  type REQ = Request
+
+  private object Initial extends DecodeState {
+    override def decode(byteBuffer: ByteBuffer): Either[Error, DecodeState] = {
+      if (byteBuffer.remaining() < 4) return Left(Error(ExceptionCode.ILLEGAL_DATA_VALUE))
+      val address = java.lang.Short.toUnsignedInt(byteBuffer.getShort)
+      val value   = byteBuffer.getShort
+
+      Right(FinalState(Request(address, value)))
+    }
+
+    override def toReq: Either[Error, Request] =
+      Left(Error(ExceptionCode.ILLEGAL_DATA_VALUE))
+  }
+
+  private case class FinalState(request: Request) extends DecodeState {
+    override def decode(byteBuffer: ByteBuffer): Either[Error, DecodeState] =
+      Left(Error(ExceptionCode.ILLEGAL_DATA_VALUE))
+
+    override def toReq: Either[Error, Request] =
+      Right(request)
+  }
+
+  override def initialDecodeState: DecodeState = Initial
 
 }
