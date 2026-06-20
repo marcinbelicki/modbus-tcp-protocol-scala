e196171f69dcbff3e88d2f7957e8be191f8f4519
Marcin Belicki <marcin.belicki@protonmail.com>
2026-06-20 16:54:18 +0200

    adding proper request reader

--- modules/models/src/main/scala/pl/belicki/modbus/models/function/MaskWriteRegister.scala
+++ modules/models/src/main/scala/pl/belicki/modbus/models/function/MaskWriteRegister.scala
@@ -1,5 +1,9 @@
 package pl.belicki.modbus.models.function
 
+import pl.belicki.modbus.models.ExceptionCode
+
+import java.nio.ByteBuffer
+
 object MaskWriteRegister extends ModbusFunction(0x16) {
 
   case class Request(
@@ -7,5 +11,36 @@ object MaskWriteRegister extends ModbusFunction(0x16) {
       andMask: Short,
       orMask: Short
   ) extends super.Request
+
+  type REQ = Request
+
+  private object Initial extends DecodeState {
+    override def decode(byteBuffer: ByteBuffer): Either[Error, DecodeState] = {
+      if (byteBuffer.remaining() != 6) return Left(Error(ExceptionCode.ILLEGAL_DATA_VALUE))
+      val address         = java.lang.Short.toUnsignedInt(byteBuffer.getShort)
+      val andMask, orMask = byteBuffer.getShort
+
+      Right(
+        FinalState(
+          Request(
+            address,
+            andMask,
+            orMask
+          )
+        )
+      )
+    }
+
+    override def toReq: Either[Error, Request] = Left(Error(ExceptionCode.ILLEGAL_DATA_VALUE))
+  }
+
+  private case class FinalState(request: Request) extends DecodeState {
+    override def decode(byteBuffer: ByteBuffer): Either[Error, DecodeState] =
+      Left(Error(ExceptionCode.ILLEGAL_DATA_VALUE))
+
+    override def toReq: Either[Error, Request] = Right(request)
+  }
+
+  override def initialDecodeState: DecodeState = Initial
 
 }
