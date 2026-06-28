930826007fefca2c18f674c65b6b51076992507b
Marcin Belicki <marcin.belicki@protonmail.com>
2026-06-28 17:23:00 +0200

    working on codes

--- modules/models/src/main/scala/pl/belicki/modbus/models/function/EncapsulatedInterfaceTransport.scala
+++ modules/models/src/main/scala/pl/belicki/modbus/models/function/EncapsulatedInterfaceTransport.scala
@@ -1,5 +1,9 @@
 package pl.belicki.modbus.models.function
 
+import pl.belicki.modbus.models.ExceptionCode
+
+import java.nio.ByteBuffer
+
 object EncapsulatedInterfaceTransport extends ModbusFunction(0x2b) {
 
   abstract class SubFunction(_code: Int) {
@@ -9,6 +13,21 @@ object EncapsulatedInterfaceTransport extends ModbusFunction(0x2b) {
     abstract class Request extends EncapsulatedInterfaceTransport.Request {
       val subFunction: SubFunction = SubFunction.this
     }
+
+  }
+
+  object SubFunction {
+    private val subFunctions = List(
+      ReadDeviceIdentification,
+      CANopenGeneralReference
+    )
+
+    val subFunctionByCode: Map[Byte, SubFunction] = subFunctions
+      .groupBy(_.code)
+      .map {
+        case (byte, subFunction :: Nil) => (byte, subFunction)
+        case (byte, subFunctions)       => throw new IllegalStateException(f"Too many subFunctions: $subFunctions for code code: 0x$byte%02X")
+      }
 
   }
 
@@ -25,6 +44,18 @@ object EncapsulatedInterfaceTransport extends ModbusFunction(0x2b) {
 
     case class Request() extends super.Request
 
+  }
+
+  type REQ = Request
+
+  private object Initial extends DecodeState {
+    override def decode(byteBuffer: ByteBuffer): Either[Error, DecodeState] = {
+      if (byteBuffer.remaining() < 1) return ExceptionCode.ILLEGAL_DATA_VALUE
+
+      val code =
+    }
+
+    override def toReq: Either[Error, Request] = ???
   }
 
 }
