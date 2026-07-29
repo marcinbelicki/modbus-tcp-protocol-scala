1d3528ac9dd6d57d63d1f9a2c4c153a0da247b3a
Marcin Belicki <marcin.belicki@protonmail.com>
2026-07-29 16:47:25 +0200

    addin decoding test

--- modules/models/src/test/scala/pl/belicki/modbus/models/DecodingTest.scala
+++ modules/models/src/test/scala/pl/belicki/modbus/models/DecodingTest.scala
@@ -11,10 +11,14 @@ import java.util.HexFormat
 class DecodingTest extends AnyWordSpecLike {
 
   "ReadCoils" must {
-    "be properly decoded" in {
-      val request = ReadCoils.decodeRequest(ByteBuffer.wrap(HexFormat.of().parseHex("00FF0001")))
+    "be properly decoded and encoded" in {
+      val Right(request) = ReadCoils.decodeRequest(ByteBuffer.wrap(HexFormat.of().parseHex("00FF0001"))): @unchecked
 
-      request shouldBe Right(ReadCoils.Request(255, 1))
+      request shouldBe ReadCoils.Request(255, 1)
+
+      val Right(byteBuffer) = request.toByteBuffer: @unchecked
+
+      byteBuffer.array() shouldBe HexFormat.of().parseHex("0100FF0001")
     }
   }
 
